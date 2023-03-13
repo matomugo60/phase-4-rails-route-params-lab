d5d605489604486ce09202bb444d932772a8515b
matomugo60 <matomugo60@gmail.com>
2023-03-13 14:55:21 +0300

    Complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      @students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    else
+      @students = Student.all
+    end
+    render json: @students
+  end
+
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
